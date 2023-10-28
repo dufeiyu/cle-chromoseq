37846eeb60e186ed9cf842a08d27398bdc086f53
Feiyu Du <fdu@wustl.edu>
2023-10-28 01:22:52 -0500

    New sample naming and NovaSeq_X_Plus in use

--- scripts/launcher.pl
+++ scripts/launcher.pl
@@ -23,41 +23,13 @@ use IO::File;
 use File::Spec;
 use File::Basename;
 
-##THIS LAUNCHER SCRIPT NEEDS TO BE RUN ON DRAGEN NODE compute1-dragen-2 TO BE ABLE TO COPY RUNDIR
-##TO LOCAL STAGING DRIVE
 die "Provide rundir, excel sample spreadsheet, and batch name in order" unless @ARGV == 3;
 
 my ($rundir, $sample_sheet, $batch_name) = @ARGV;
+die "$rundir is not valid" unless -d $rundir;
 die "$sample_sheet is not valid" unless -s $sample_sheet;
 
-my $staging_rundir = '/staging/runs/Chromoseq/rundir';
-my $scratch_rundir = '/scratch1/fs1/duncavagee/Chromoseq/rundir';
 my $dir = '/storage1/fs1/duncavagee/Active/SEQ/Chromoseq';
-
-if ($rundir =~ /$staging_rundir|$scratch_rundir/) {
-    print "$rundir is ready\n";
-}
-else {
-    die "Run on dragen node compute1-dragen-2 !" unless -d $staging_rundir;
-    #chdir($staging_rundir) or die "cannot change: $!\n";
-    die "$rundir is not valid" unless -d $rundir;
-    $rundir =~ s/\/*$//;
-    dircopy($rundir, $staging_rundir) or die $!;
-    #my $rv = system "/bin/cp -a $rundir ./";
-    #my $rv = system "/usr/bin/rsync -avW $rundir $staging_rundir";
-    #unless ($rv == 0) {
-    #    die "Failed to cp $rundir to $staging_rundir\n";
-    #}
-    print "Copying to $staging_rundir is DONE\n";
-    $rundir = File::Spec->join($staging_rundir, basename($rundir));
-    #chdir($dir) or die "cannot change: $!\n";
-}
-
-my $run_xml = File::Spec->join($rundir, 'RunParameters.xml');
-unless (-s $run_xml) {
-    die "RunParameters.xml $run_xml is not valid";
-}
-
 my $git_dir = File::Spec->join($dir, 'process', 'git', 'cle-chromoseq');
 
 my $conf = File::Spec->join($git_dir, 'application.conf');
@@ -66,8 +38,8 @@ my $zip  = File::Spec->join($git_dir, 'imports.zip');
 my $json_template = File::Spec->join($git_dir, 'inputs.json');
 
 my $group  = '/cle/wdl/chromoseq';
-my $queue  = 'pathology';
-my $docker = 'registry.gsc.wustl.edu/apipe-builder/genome_perl_environment:compute1-20';
+my $queue  = 'dspencer';
+my $docker = 'mgibio/genome_perl_environment:compute1-38';
 
 my $user_group = 'compute-duncavagee';
 
@@ -78,142 +50,173 @@ unless (-d $out_dir) {
     }
 }
 
-## DRAGEN sample sheet
-my $dragen_ss  = File::Spec->join($out_dir, 'sample_sheet.csv'); 
-my $ss_fh = IO::File->new(">$dragen_ss") or die "fail to write to $dragen_ss";
-$ss_fh->print("[Data]\n");
-$ss_fh->print("Lane,Sample_ID,Sample_Name,Sample_Project,index,index2\n");
+#parsing CoPath dump for MRN, ACCESSION, DOB and Gender
+my $copath_dump = '/storage1/fs1/duncavagee/Active/SEQ/Chromoseq/process/daily_accession/WML_Daily_Accession_Log_CLE.csv';
+my $dump_fh = IO::File->new($copath_dump) or die "fail to open $copath_dump for reading";
+my %hash;
 
+while (my $l = $dump_fh->getline) {
+    next if $l =~ /^(Accession|,,,,)/;
+    next unless $l =~ /ChromoSeq\s/;
+    
+    my @columns = split /,/, $l;
+    my $id  = $columns[0];
+    my $sex = $columns[4];
+
+    if ($sex eq 'M') {
+        $sex = 'male';
+    }
+    elsif ($sex eq 'F') {
+        $sex = 'female';
+    }
+    else {
+        #Gender U
+        warn "WARN: unknown gender $sex found for $id";
+    }
+
+    $hash{$id} = {
+        MRN => $columns[2],
+        DOB => $columns[5],
+        sex => $sex,
+    };
+}
+$dump_fh->close;
+
+#parse sample spreadsheet
 my $data = Spreadsheet::Read->new($sample_sheet);
 my $sheet = $data->sheet(1);
 
 my %info;
+my $str;
 
 for my $row ($sheet->rows()) {
     next if $row->[0] =~ /Run|Lane/i;
     unless ($row->[0] =~ /\d+/) {
         die "Lane number is expected, Check sample sheet spreadsheet";
     }
-    my ($lane, undef, $lib, $DOB, $sex, $index1, $index2, $exception) = @$row;
+    my ($lane_str, undef, $name, $index, $exception) = @$row;
 
-    $lib =~ s/\s+//g;
-    if ($lib =~ /^[A-Z]{4}\-(\d+)\-[A-Z]\d+\-\d+\-[A-Z0-9]+\-lib/) {
-        unless ((length($1) == 10 and $1 =~ /^99/) or (length($1) == 9 and $1 =~ /^1/)) {
-            die "$lib has invalid MRN: $1  MRN must be either a 10-digit number starting with 99 or a 9-digit number starting with 1";
-        }
+    $name =~ s/\s+//g;
+    my ($accession) = $name =~ /^(W\d+\-\d+)\-/;
+    unless ($accession) {
+        die "$name must include accession id";
     }
-    else {
-        die "Library name: $lib must contain MRN, accession id and specimen type";
-    }
-
-    unless ($DOB =~ /\d{2}\/\d{2}\/\d{4}/) {
-        die "Date of birth: $DOB has invalid format. It need be mm/dd/yyyy";
+    unless (exists $hash{$accession}) {
+        die "Fail to get info from CoPath daily Active log for $accession of $name";
     }
 
+    my @lib_name = ('TWDY', $hash{$accession}->{MRN}, $name);
+    unless ($name =~ /lib\d+$/) {
+        push @lib_name, 'lib1';
+    }
+    my $lib = join '-', @lib_name;
 
-    unless ($sex =~ /^(male|female)$/) {
-        die "$lib sex has to be either male or female, not:$sex";
+    $lane_str =~ s/\s+//g;
+    my @lanes = split /,/, $lane_str;
+
+    my ($index1, $index2) = $index =~ /([ATGC]{10})\-([ATGC]{10})/;
+    
+    for my $lane (@lanes) {
+        $str .= join ',', $lane, $lib, $lib, '', $index1, $index2;
+        $str .= "\n";
     }
 
-    my ($clean_index1) = $index1 =~ /([ACGT]+)/;
-    my ($clean_index2) = $index2 =~ /([ACGT]+)/;
-
-    unless (length($clean_index1) =~ /^(8|10)$/ and length($clean_index2) =~ /^(8|10)$/) {
-        die "Provided $lib index1:$index1 and or index2:$index2 do not have 8 or 10 bases";
-    }
-
-    my $fix_index2 = rev_comp($clean_index2);
     $exception = 'NONE' unless $exception;
-
-    $ss_fh->print(join ',', $lane, $lib, $lib, '', $clean_index1, $fix_index2);
-    $ss_fh->print("\n");
-
     $info{$lib} = {
-        DOB => $DOB,
-        sex => $sex,
+        DOB => $hash{$accession}->{DOB},
+        sex => $hash{$accession}->{sex},
         exception => $exception,
     };
 }
-$ss_fh->close;
 
-## Input JSON
-my ($runid, $R1cycle, $R2cycle, $index1cycle, $index2cycle, $fcmode, $wftype, $instr, $side);
+## Get RunInfoString
+my $run_xml = File::Spec->join($rundir, 'RunParameters.xml');
+unless (-s $run_xml) {
+    die "RunParameters.xml $run_xml is not valid";
+}
 my $xml_fh = IO::File->new($run_xml) or die "Fail to open $run_xml";
+my ($runid, $R1cycle, $R2cycle, $index1cycle, $index2cycle, $fcmode, $wftype, $instr, $side);
+my $fctype = 0;
 
 while (my $line = $xml_fh->getline) {
     if ($line =~ /<RunId>(\S+)<\/RunId>/) {
         $runid = $1;
     }
-    elsif ($line =~ /<Read1NumberOfCycles>(\d+)<\/Read1NumberOfCycles>/) {
+    elsif ($line =~ / ReadName="Read1" Cycles="(\d+)" /) {
         $R1cycle = $1;
     }
-    elsif ($line =~ /<Read2NumberOfCycles>(\d+)<\/Read2NumberOfCycles>/) {
+    elsif ($line =~ / ReadName="Read2" Cycles="(\d+)" /) {
         $R2cycle = $1;
     }
-    elsif ($line =~ /<IndexRead1NumberOfCycles>(\d+)<\/IndexRead1NumberOfCycles>/) {
+    elsif ($line =~ / ReadName="Index1" Cycles="(\d+)" /) {
         $index1cycle = $1;
     }
-    elsif ($line =~ /<IndexRead2NumberOfCycles>(\d+)<\/IndexRead2NumberOfCycles>/) {
+    elsif ($line =~ / ReadName="Index2" Cycles="(\d+)" /) {
         $index2cycle = $1;
     }
-    elsif ($line =~ /<FlowCellMode>(\S+)<\/FlowCellMode>/) {
-        $fcmode = $1;
-    }
-    elsif ($line =~ /<WorkflowType>(\S+)<\/WorkflowType>/) {
+    elsif ($line =~ /<InstrumentType>(\S+)<\/InstrumentType>/) {
         $wftype = $1;
     }
-    elsif ($line =~ /<InstrumentName>(\S+)<\/InstrumentName>/) {
+    elsif ($line =~ /<InstrumentSerialNumber>(\S+)<\/InstrumentSerialNumber>/) {
         $instr = $1;
     }
     elsif ($line =~ /<Side>(\S+)<\/Side>/) {
         $side = $1;
     }
+    elsif ($line =~ /<Type>FlowCell<\/Type>/) {
+        $fctype = 1;
+    }
+    if ($fctype) {
+        if ($line =~ /<Mode>(\S+)<\/Mode>/) {
+            $fcmode = $1;
+            $fctype = 0;
+        }
+    }
 }
 $xml_fh->close;
-
 my $run_info_str = join ',', $runid, $instr, $side, $fcmode, $wftype, $R1cycle, $index1cycle, $index2cycle, $R2cycle; 
 
-my $inputs = from_json(`cat $json_template`);
-$inputs->{'ChromoSeq.RunInfoString'} = $run_info_str;
-$inputs->{'ChromoSeq.RunDir'} = $rundir;
-$inputs->{'ChromoSeq.SampleSheet'} = $dragen_ss;
-$inputs->{'ChromoSeq.Batch'} = $batch_name;
+## DRAGEN sample sheet
+my $dragen_ss  = File::Spec->join($out_dir, 'sample_sheet.csv'); 
+my $ss_fh = IO::File->new(">$dragen_ss") or die "fail to write to $dragen_ss";
 
-my @samples;
-my @genders;
-my @exceptions;
-my @DOBs;
-
-for my $sample (sort keys %info) {
-    push @samples, $sample;
-    push @DOBs, $info{$sample}->{DOB};
-    push @genders, $info{$sample}->{sex};
-    push @exceptions, $info{$sample}->{exception};
+if ($index1cycle == 19) {
+    print "Index1 has 19 cycles!\n";
+    $ss_fh->print("[Settings]\n");
+    $ss_fh->print("OverrideCycles,Y151;I10N9;I10;Y151\n");
 }
 
-$inputs->{'ChromoSeq.Samples'} = \@samples;
-$inputs->{'ChromoSeq.Genders'} = \@genders;
-$inputs->{'ChromoSeq.Exceptions'} = \@exceptions;
-$inputs->{'ChromoSeq.DOBs'} = \@DOBs;
+$ss_fh->print("[Data]\n");
+$ss_fh->print("Lane,Sample_ID,Sample_Name,Sample_Project,index,index2\n");
+$ss_fh->print($str);
+$ss_fh->close;
+
+## Input JSON
+my $inputs = from_json(`cat $json_template`);
+
+$inputs->{'ChromoSeq.Batch'}         = $batch_name;
+$inputs->{'ChromoSeq.RunDir'}        = $rundir;
+$inputs->{'ChromoSeq.SampleSheet'}   = $dragen_ss;
+$inputs->{'ChromoSeq.RunInfoString'} = $run_info_str;
+
+my @samples = (sort keys %info);
+
+$inputs->{'ChromoSeq.Samples'}    = \@samples;
+$inputs->{'ChromoSeq.DOBs'}       = [map{$info{$_}->{DOB}}@samples];
+$inputs->{'ChromoSeq.Genders'}    = [map{$info{$_}->{sex}}@samples];
+$inputs->{'ChromoSeq.Exceptions'} = [map{$info{$_}->{exception}}@samples];
 
 my $input_json = File::Spec->join($out_dir, 'inputs.json');
 my $json_fh = IO::File->new(">$input_json") or die "fail to write to $input_json";
 
-$json_fh->print(to_json($inputs, {pretty => 1}));
+$json_fh->print(to_json($inputs, {canonical => 1, pretty => 1}));
 $json_fh->close;
 
 my $out_log = File::Spec->join($out_dir, 'out.log');
 my $err_log = File::Spec->join($out_dir, 'err.log');
 
-my $cmd = "bsub -g $group -G $user_group -oo $out_log -eo $err_log -q $queue -a \"docker($docker)\" /usr/bin/java -Dconfig.file=$conf -jar /opt/cromwell.jar run -t wdl --imports $zip -i $input_json $wdl";
+my $cmd = "bsub -g $group -G $user_group -oo $out_log -eo $err_log -q $queue -R \"select[mem>16000] rusage[mem=16000]\" -M 16000000 -a \"docker($docker)\" /usr/bin/java -Dconfig.file=$conf -jar /opt/cromwell.jar run -t wdl --imports $zip -i $input_json $wdl";
 
 system $cmd;
 #print $cmd."\n";
 
-sub rev_comp {
-    my $index = shift;
-    my $revcomp = reverse $index;
-    $revcomp =~ tr/ATGCatgc/TACGtacg/;
-
-    return $revcomp;
-}
